0200e1e81270a19bd31ab4e5438bb2f92abca279
joeornstein <jornstein@gmail.com>
2023-02-03 17:16:23 -0500

    document few-shot examples

--- R/data.R
+++ R/data.R
@@ -23,3 +23,25 @@
 #' @examples
 #' data(scotus_tweets)
 "scotus_tweets"
+
+#' Labelled Example Tweets About The Supreme Court of the United States
+#'
+#' This dataset contains 5 example tweets referencing the Supreme Court
+#' along with a sentiment label. These can be used as few-shot learning
+#' examples for classifying tweets in the `scotus_tweets` dataset.
+#'
+#' @docType data
+#'
+#' @usage data(scotus_tweet_examples)
+#'
+#' @format
+#' A data frame with 5 rows and 2 columns:
+#' \describe{
+#'  \item{text}{The text of the tweet}
+#'  \item{label}{The "true" label (Positive, Negative, or Neutral)}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @references Ornstein et al. (2022). "How To Train Your Stochastic Parrot"
+"scotus_tweet_examples"
