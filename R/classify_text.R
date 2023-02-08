aa75b274de68ee3cdcbaa2a290d024c4f2ed49af
joeornstein <jornstein@gmail.com>
2023-02-08 10:17:05 -0500

    fix error message typo

--- R/classify_text.R
+++ R/classify_text.R
@@ -25,7 +25,7 @@ classify_text <- function(text,
   openai <- reticulate::import("openai")
 
   if(is.null(openai_api_key)){
-    stop("No API key detected in system environment. You can enter it manually using 'the openai_api_key' argument.")
+    stop("No API key detected in system environment. You can enter it manually using the 'openai_api_key' argument.")
   } else{
     openai$api_key = openai_api_key
   }
