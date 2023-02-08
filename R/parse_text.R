a7a153f00b452298c74c08678e42e2fb17a7bbd2
joeornstein <jornstein@gmail.com>
2023-02-08 11:27:15 -0500

    fix max tokens

--- R/parse_text.R
+++ R/parse_text.R
@@ -5,7 +5,7 @@
 #' @param text The text to be converted into a tidy dataframe
 #' @param instructions Instructions to be included in the GPT-3 prompt (format them like you would format instructions to a human research assistant, in as much detail as possible).
 #' @param var_descriptions A list of descriptions for each variable in the dataframe.
-#' @param col_names Optional: A list of column names for the resulting dataframe. If not provided, var_descriptions will be used for column names.
+#' @param col_names Optional: A list of column names for the resulting dataframe. Must be the same length as the list of variable descriptions. If not provided, var_descriptions will be used for column names.
 #' @param model Which model variant of GPT-3 to use. Defaults to 'text-davinci-003'
 #' @param openai_api_key Your API key. By default, looks for a system environment variable called "OPENAI_API_KEY" (recommended option). Otherwise, it will prompt you to enter the API key as an argument.
 #'
@@ -43,9 +43,10 @@ parse_text <- function(text,
   # query the API
   response <- openai$Completion$create(
     engine = model,
-    prompt = paste0(instructions, "\n\n", text, "\n\n", paste0('| ', var_descriptions, collapse = ''), " |\n|",
-                    paste0( rep(' --- |', length(var_descriptions)), collapse = ''), "\n"),
-    temperature = as.integer(0)
+    prompt = paste0(instructions, "\n\n", text, "\n\n", paste0('| ', var_descriptions, ' ', collapse = ''), "|\n|",
+                    paste0( rep(' --- |', length(var_descriptions)), collapse = ''), "\n| "),
+    temperature = as.integer(0),
+    max_tokens = as.integer(1000)
   )
 
 
@@ -66,6 +67,10 @@ parse_text <- function(text,
     dplyr::mutate_all(stringr::str_squish) |>
     # remove that last row
     dplyr::slice(1:dplyr::n() - 1)
+
+  if(length(col_names) != 0 & length(var_descriptions) != length(col_names)){
+    stop('Error: col_names and var_descriptions must be the same length.')
+  }
 
   if(length(col_names) == length(var_descriptions)){
     names(d) <- col_names
