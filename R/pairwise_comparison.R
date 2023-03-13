6dec1af7ca1048c08c630bada2eb38b6d6e40923
Joseph Ornstein <jo22058@uga.edu>
2023-03-13 08:55:04 -0400

    return most likely response

--- R/pairwise_comparison.R
+++ R/pairwise_comparison.R
@@ -6,7 +6,7 @@
 #' @param model Which model variant of GPT-3 to use. Defaults to 'text-davinci-003'
 #' @param openai_api_key Your API key. By default, looks for a system environment variable called "OPENAI_API_KEY" (recommended option). Otherwise, it will prompt you to enter the API key as an argument.
 #'
-#' @return The probability GPT-3 chooses Option A
+#' @return A character (A or B)
 #' @export
 #'
 #' @examples
@@ -30,32 +30,40 @@ pairwise_comparison <- function(A, B,
   # query the API
   response <- openai$Completion$create(
     engine = model,
-    prompt = paste0(instructions, '\n---\nA. ', A, '\n\nB. ', B, '\n---\nResponse:'),
-    max_tokens = as.integer(1),
+    prompt = paste0(instructions, '\n---\nA. ', A, '\n\nB. ', B, '\n---\nResponse (A or B):'),
+    max_tokens = as.integer(10),
     logprobs = as.integer(5),
     temperature = as.integer(0)
   )
 
-  # convert the dictionary of log probabilities into a dataframe
-  keys <- names(response$choices[[1]]$logprobs$top_logprobs[[1]])
 
-  logprobs <- numeric(length(keys))
+  # this code returns the highest probability output string
+  response$choices[[1]]$text |>
+    # remove carriage returns and whitespace
+    stringr::str_squish()
 
-  for(i in 1:length(keys)){
-    logprobs[i] <- response$choices[[1]]$logprobs$top_logprobs[[1]]$get(keys[i])
-  }
 
-  # tidy the dataframe
-  data.frame(response = stringr::str_squish(keys),
-                  prob = exp(logprobs)) |>
-    dplyr::filter(response %in% c('A', 'B')) |>
-    dplyr::mutate(response = factor(response, level = c('A', 'B'))) |>
-    dplyr::group_by(response, .drop = FALSE) |>
-    dplyr::summarize(prob = sum(prob)) |>
-    dplyr::ungroup() |>
-    # convert to proper probability (sums to 1)
-    dplyr::mutate(prob = prob / sum(prob)) |>
-    dplyr::filter(response == 'A') |>
-    dplyr::pull(prob)
+  # the commented out code returns a probability
+  # # convert the dictionary of log probabilities into a dataframe
+  # keys <- names(response$choices[[1]]$logprobs$top_logprobs[[1]])
+  #
+  # logprobs <- numeric(length(keys))
+  #
+  # for(i in 1:length(keys)){
+  #   logprobs[i] <- response$choices[[1]]$logprobs$top_logprobs[[1]]$get(keys[i])
+  # }
+  #
+  # # tidy the dataframe
+  # data.frame(response = stringr::str_squish(keys),
+  #                 prob = exp(logprobs)) |>
+  #   dplyr::filter(response %in% c('A', 'B')) |>
+  #   dplyr::mutate(response = factor(response, level = c('A', 'B'))) |>
+  #   dplyr::group_by(response, .drop = FALSE) |>
+  #   dplyr::summarize(prob = sum(prob)) |>
+  #   dplyr::ungroup() |>
+  #   # convert to proper probability (sums to 1)
+  #   dplyr::mutate(prob = prob / sum(prob)) |>
+  #   dplyr::filter(response == 'A') |>
+  #   dplyr::pull(prob)
 
 }
