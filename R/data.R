ef7b201a5c387842b3d67c1b22a26e5fd8bc3588
Joe Ornstein <jornstein@uga.edu>
2023-03-14 11:08:10 -0400

    content warning

--- R/data.R
+++ R/data.R
@@ -1,8 +1,13 @@
 #' Tweets About The Supreme Court of the United States
 #'
-#' This dataset contains 945 tweets referencing the Supreme Court,
-#' collected between XXXX and YYYY, along with expert-coded sentiment
-#' scores.
+#' This dataset contains 945 tweets referencing the US Supreme Court.
+#' Roughly half were collected on June 4, 2018 following the *Masterpiece Cakeshop*
+#' ruling, and the other half were collected on July 9, 2020 following the
+#' Court's concurrently released opinions in *Trump v. Mazars* and *Trump v. Vance*.
+#' Each tweet includes three independent human-coded sentiment scores (-1 to +1).
+#'
+#' CONTENT WARNING: These texts come from social media, and many contain explicit
+#' or offensive language.
 #'
 #' @docType data
 #'
@@ -19,16 +24,13 @@
 #' @keywords datasets
 #'
 #' @references Ornstein et al. (2022). "How To Train Your Stochastic Parrot"
-#'
-#' @examples
-#' data(scotus_tweets)
 "scotus_tweets"
 
 
 #' Labelled Example Tweets About The Supreme Court of the United States
 #'
 #' This dataset contains 5 example tweets referencing the Supreme Court
-#' along with a sentiment label. These can be used as few-shot learning
+#' along with a sentiment label. These can be used as few-shot prompting
 #' examples for classifying tweets in the `scotus_tweets` dataset.
 #'
 #' @docType data
