064391718e3903643a32e84c8b33757d90676c30
Joe Ornstein <jornstein@uga.edu>
2023-08-09 13:43:03 -0400

    use chat endpoint

--- R/parse_text.R
+++ R/parse_text.R
@@ -1,27 +1,24 @@
 #' Create Dataframe From Text
 #'
-#' @description Constructs a GPT-3 prompt to parse data from unstructured text according to the user's instructions.
+#' @description Constructs a data table from unstructured text according to the user's instructions.
 #'
 #' @param text The text to be converted into a tidy dataframe
 #' @param instructions Instructions to be included in the GPT-3 prompt (format them like you would format instructions to a human research assistant, in as much detail as possible).
-#' @param var_descriptions A list of descriptions for each variable in the dataframe.
-#' @param col_names Optional: A list of column names for the resulting dataframe. Must be the same length as the list of variable descriptions. If not provided, var_descriptions will be used for column names.
-#' @param model Which model variant of GPT-3 to use. Defaults to 'text-davinci-003'
+#' @param col_names A list of column names for each variable in the dataframe.
+#' @param model Which model variant of GPT-3 to use. Defaults to 'gpt-3.5-turbo'
 #' @param openai_api_key Your API key. By default, looks for a system environment variable called "OPENAI_API_KEY" (recommended option). Otherwise, it will prompt you to enter the API key as an argument.
 #'
-#' @return A dataframe with a number of columns equal to `ncol(var_descriptions)`
+#' @return A dataframe with a number of columns equal to `ncol(col_names)`
 #' @export
 #'
 #' @examples
 #' parse_text(text = "John went to the store. Rosie ran errands at the mall.",
 #'            instructions = "Create a dataset from the following passage with the names of each character and the place they went.",
-#'            var_descriptions = c("Name of Character", "Where They Went"),
-#'            col_names = c('name', 'destination') )
+#'            col_names = c("Name of Character", "Where They Went"))
 parse_text <- function(text,
                        instructions,
-                       var_descriptions,
-                       col_names = character(0),
-                       model = 'text-davinci-003',
+                       col_names,
+                       model = 'gpt-3.5-turbo',
                        openai_api_key = Sys.getenv('OPENAI_API_KEY')) {
 
   openai <- reticulate::import("openai")
@@ -32,34 +29,47 @@ parse_text <- function(text,
     openai$api_key = openai_api_key
   }
 
-  # if(length(var_descriptions) == 0){
-  #   stop("Please provide a list of descriptions for each variable in the dataframe.")
-  # }
-  #
-  # if(instructions == ''){
-  #   stop("Please provide instructions (in plain English) describing in as much detail as possible the dataframe you would like to create.")
-  # }
+  # format the prompt
+  prompt <- paste0(instructions,
+                  "\n---\n",
+                  text,
+                  "\n---\n\n",
+                  paste0("| ", col_names, " ", collapse = ""),
+                  "|\n|",
+                  paste0(rep(" --- |", length(col_names)),
+                         collapse = ""),
+                  "\n")
+
+  # for Chat Endpoint, embed the prompt in a "messages" dictionary object
+  messages <- reticulate::dict(role = 'user', content = prompt)
 
   # query the API
-  response <- openai$Completion$create(
-    engine = model,
-    prompt = paste0(instructions, "\n---\n", text, "\n---\n\n", paste0('| ', var_descriptions, ' ', collapse = ''), "|\n|",
-                    paste0( rep(' --- |', length(var_descriptions)), collapse = ''), "\n"),
-    temperature = as.integer(0),
-    max_tokens = as.integer(1000)
-  )
+  response <- openai$ChatCompletion$create(model = model,
+                                           messages = c(messages),
+                                           temperature = as.integer(0),
+                                           max_tokens = as.integer(2000))
+
+  output <- response$choices[[1]]$message$content
 
 
-  output <- response$choices[[1]]$text
+  # deprecated Completions endpoint
+  # response <- openai$Completion$create(
+  #   engine = model,
+  #   prompt = paste0(instructions, "\n---\n", text, "\n---\n\n", paste0('| ', var_descriptions, ' ', collapse = ''), "|\n|",
+  #                   paste0( rep(' --- |', length(var_descriptions)), collapse = ''), "\n"),
+  #   temperature = as.integer(0),
+  #   max_tokens = as.integer(1000)
+  # )
+  # output <- response$choices[[1]]$text
 
   # convert the resulting table to a tidy dataframe
   d <- readr::read_delim(
     # paste an extra row, in case there's only one row.
     paste0(output, '\n', paste(rep(
-      '|', length(var_descriptions) + 1
+      '|', length(col_names) + 1
     ), collapse = '')),
     delim = '|',
-    col_names = c('empty1', var_descriptions, 'empty2')
+    col_names = c('empty1', col_names, 'empty2')
   ) |>
     # read_delim sees the leading and trailing |'s as marking empty columns. remove those columns
     dplyr::select(-empty1,-empty2) |>
@@ -67,14 +77,6 @@ parse_text <- function(text,
     dplyr::mutate_all(stringr::str_squish) |>
     # remove that last row
     dplyr::slice(1:dplyr::n() - 1)
-
-  if(length(col_names) != 0 & length(var_descriptions) != length(col_names)){
-    stop('Error: col_names and var_descriptions must be the same length.')
-  }
-
-  if(length(col_names) == length(var_descriptions)){
-    names(d) <- col_names
-  }
 
   return(d)
 
